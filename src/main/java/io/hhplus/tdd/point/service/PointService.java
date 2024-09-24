ca848e83ca81f46a299d0dce72bd267dafc4789e
hyojin202406 <hyojin202406@gmail.com>
2024-09-25 00:40:59 +0900

    feat(point): 유저 포인트 사용 기능 추가
    - 유저 포인트 충전 기능 추가

--- src/main/java/io/hhplus/tdd/point/service/PointService.java
+++ src/main/java/io/hhplus/tdd/point/service/PointService.java
@@ -6,6 +6,7 @@ import io.hhplus.tdd.point.domain.TransactionType;
 import io.hhplus.tdd.point.domain.UserPoint;
 import io.hhplus.tdd.point.exception.InvalidUserIdException;
 import io.hhplus.tdd.point.exception.PointValidationException;
+import io.hhplus.tdd.point.exception.UserPointNotFoundException;
 import io.hhplus.tdd.point.repository.PointRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -81,6 +82,54 @@ public class PointService {
 
         // 변경된 유저 정보 전달
         return updatedUserPoint;
+    }
+
+    /**
+     * 유저 포인트 사용 기능
+     * @param id
+     * @param amount
+     * @return
+     */
+    public UserPoint useUserPoint(long id, long amount) {
+        // 유효하지 않은 ID 검사
+        if (id <= 0) {
+            throw new InvalidUserIdException("Invalid user ID: " + id);
+        }
+
+        // 유효하지 않은 포인트 처리
+        if (amount < 0) {
+            throw new PointValidationException("Point validation exception. amount : " + amount);
+        }
+
+        // 유저 포인트 조회
+        UserPoint currentUserPoint = pointRepository.getUserPoint(id);
+
+        // 유저 포인트 조회 실패 시 예외 처리
+        if (currentUserPoint == null) {
+            throw new UserPointNotFoundException("Failed to find user point for ID: " + id);
+        }
+
+        // 남은 포인트 계산
+        long remainingPoint = currentUserPoint.point() - amount;
+
+        // 포인트 부족 시 예외 처리
+        if (remainingPoint < 0) {
+            throw new PointValidationException("Point validation exception. remainingPoint : " + remainingPoint);
+        }
+
+        // 포인트 업데이트 및 변환
+        UserPoint userUpdatedPoint = pointRepository.insertOrUpdate(id, remainingPoint);
+
+        // 포인트 사용 내역 저장
+        pointRepository.insertHistory(
+                id,
+                amount,
+                TransactionType.USE,
+                userUpdatedPoint.updateMillis()
+        );
+
+        // 업데이트된 포인트 반환
+        return userUpdatedPoint;
     }
 
 }
